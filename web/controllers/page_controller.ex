74a0f66dc2313631d372adbe12fdb383dd5b8f0d
Wendy Smoak <wsmoak@gmail.com>
2015-07-03 16:57:43 -0400

    Try out Logger

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
+require Logger
+
 defmodule OAuth2Example.PageController do
   use OAuth2Example.Web, :controller
 
   plug :action
 
   def index(conn, _params) do
+    Logger.info "Displaying index page"
     render conn, "index.html"
   end
 end
