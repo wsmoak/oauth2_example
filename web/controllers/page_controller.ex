6460a88deadb066c323e3dfdc60b4dbd2f797c7e
Wendy Smoak <wsmoak@gmail.com>
2015-07-07 06:20:50 -0400

    Log the connection (which includes the session) after the redirect to /

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,9 +1,13 @@
+require Logger
+
 defmodule OAuth2Example.PageController do
   use OAuth2Example.Web, :controller
 
   plug :action
 
   def index(conn, _params) do
+    Logger.debug "Displaying index page"
+    Logger.debug inspect(conn)
     render conn, "index.html"
   end
 end
